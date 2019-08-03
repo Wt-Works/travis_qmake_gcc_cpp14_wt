55a9ad284e4579f09a621160917df76b6950524d
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-03 14:32:59 +0200

    Remove pragma's

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#pragma GCC diagnostic push
+
 
 #include <boost/program_options.hpp>
 #include <boost/signals2.hpp>
@@ -9,7 +9,7 @@
 #include <Wt/WPaintedWidget>
 #include <Wt/WPainter>
 #include <Wt/WPushButton>
-#pragma GCC diagnostic pop
+
 
 //A C++14 construct
 auto f() noexcept {
